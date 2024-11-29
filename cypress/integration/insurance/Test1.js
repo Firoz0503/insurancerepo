a5a88825914bbc8c5206d555ecb7923a96ce8b27
Firoz <firoz.ahmed.testing@gmail.com>
2024-11-29 15:50:55 +0530

    First Commit

---
+++ cypress/integration/insurance/Test1.js
@@ -0,0 +1,39 @@
+describe('Insurance Project', () => {
+    before(function () {
+
+        cy.fixture('example1').then(function (data) {
+            this.data = data
+        })
+
+    })
+    it('Insurance Framework', function () {
+        cy.visit('https://demo.guru99.com/insurance/v1/index.php')
+        cy.get('input[name="email"]').type(this.data.Email)
+        cy.get('input[name="password"]').type(this.data.Password)
+        //cy.contains("Log in").click()
+        cy.get('input[value="Log in"]').click()
+
+           //cy.origin("https://demo.guru99.com/insurance/v1/header.php", () => {
+        //})
+        cy.get('#newquote').click()
+        cy.get('select').invoke('val', '2')
+        cy.get('input[value="No"]').click()
+        cy.get('input[name="incidents"]').type(this.data.Incidents)
+        cy.get('input[name="registration"]').type(this.data.Registration)
+        cy.get('input[name="mileage"]').type(this.data.Mileage)
+        cy.get('input[name="value"]').type(this.data.Value)
+        
+        cy.get('#quotation_vehicle_attributes_parkinglocation').select('Driveway/Carport')
+        cy.get('#quotation_vehicle_attributes_policystart_1i').select('2016')
+        cy.get('#quotation_vehicle_attributes_policystart_2i').select('January')
+        cy.get('#quotation_vehicle_attributes_policystart_3i').select('15')
+        cy.get('input[value="Calculate Premium"]').click()
+        cy.get('#calculatedpremium').should('be.visible')
+        cy.get('input[value="Save Quotation').should('be.visible')
+        cy.get('input[value="Save Quotation').should('not.be.disabled').click()
+        cy.get('body b').eq(1).contains('Your identification number is :')
+        cy.get('body b').eq(1).should('contain','Your identification number is :')
+     
+    })
+})
+
---
+++ cypress/integration/insurance/Test3.js
@@ -0,0 +1,39 @@
+describe('Insurance Project', () => {
+    before(function () {
+
+        cy.fixture('example3').then(function (data) {
+            this.data = data
+        })
+
+    })
+    it('Insurance Framework', function () {
+        cy.visit('https://demo.guru99.com/insurance/v1/index.php')
+        cy.get('input[name="email"]').type(this.data.Email)
+        cy.get('input[name="password"]').type(this.data.Password)
+        //cy.contains("Log in").click()
+        cy.get('input[value="Log in"]').click()
+
+           //cy.origin("https://demo.guru99.com/insurance/v1/header.php", () => {
+        //})
+        cy.get('#newquote').click()
+        cy.get('select').invoke('val', '4')
+        cy.get('input[value="Yes"]').click()
+        cy.get('input[name="incidents"]').type(this.data.Incidents)
+        cy.get('input[name="registration"]').type(this.data.Registration)
+        cy.get('input[name="mileage"]').type(this.data.Mileage)
+        cy.get('input[name="value"]').type(this.data.Value)
+        
+        cy.get('#quotation_vehicle_attributes_parkinglocation').select('Unlocked Garage')
+        cy.get('#quotation_vehicle_attributes_policystart_1i').select('2020')
+        cy.get('#quotation_vehicle_attributes_policystart_2i').select('July')
+        cy.get('#quotation_vehicle_attributes_policystart_3i').select('12')
+        cy.get('input[value="Calculate Premium"]').click()
+        cy.get('#calculatedpremium').should('be.visible')
+        cy.get('input[value="Save Quotation').should('be.visible')
+        cy.get('input[value="Save Quotation').should('not.be.disabled').click()
+        cy.get('body b').eq(1).contains('Your identification number is :')
+        cy.get('body b').eq(1).should('contain','Your identification number is :')
+     
+    })
+})
+
